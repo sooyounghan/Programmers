ab478cfc8fdcb1288a58227f8040d38f24424025
sooyounghan <34672301+sooyounghan@users.noreply.github.com>
2024-02-03 18:56:21 +0900

    Create 배열 만들기 3(ArrayList).java

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3(ArrayList).java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+
+        for(int i = 0; i < intervals.length; i++) {
+            int idx_a = intervals[i][0];
+            int idx_b = intervals[i][1];
+
+            for(int j = idx_a; j <= idx_b; j++) {
+                result.add(arr[j]);
+            }
+        }
+
+        int[] answer = new int[result.size()];
+        for(int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
+        }
+        return answer;
+    }
+}
