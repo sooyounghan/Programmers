f2ee5357c8f7f44ceafa989cacf4b37f5bfff3d9
LoveWinsAll <34672301+sooyounghan@users.noreply.github.com>
2024-02-07 08:35:08 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 9.22 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        for(int i = 0; i < myString.length(); i++) {
+            char ch = myString.charAt(i);
+            
+            if(ch >= 'A' && ch <= 'Z') {
+                ch += 32;
+                answer += ch;
+            }
+            else answer += ch;
+        }
+        return answer;
+    }
+}
