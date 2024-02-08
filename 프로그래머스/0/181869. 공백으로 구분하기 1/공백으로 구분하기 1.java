077cf8ba77f66f69301d5bc6f9b156ecf45b2ae0
LoveWinsAll <34672301+sooyounghan@users.noreply.github.com>
2024-02-08 10:51:55 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.01 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = {};
+        int count = 0;
+        
+        for(int i = 0; i < my_string.length(); i++) {
+            if(my_string.charAt(i) == ' ') 
+                count++;
+        }
+
+        answer = new String[count + 1];
+        if(count == 0) {
+            answer[0] = my_string;
+        }
+        
+        else {
+            for(int i = 0, idx = 0, j = 0; i < my_string.length(); i++) {
+                if(my_string.charAt(i) == ' ') {
+                   answer[j++] = my_string.substring(idx, i);
+                   idx = i + 1; 
+                }
+                
+                if(i == my_string.length() - 1) {
+                    answer[j++] = my_string.substring(idx, my_string.length());  
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
