b5c73e398a30cfb1ad5bc955e0e272371be64d6d
LoveWinsAll <34672301+sooyounghan@users.noreply.github.com>
2024-02-10 00:49:29 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 46.25 ms, Memory: 96.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String myString) {
+        String[] answer = {};
+        String temp = "";
+        int count = 0;
+        
+        for(int i = 0; i < myString.length(); i++) {
+            if(i == myString.length() - 1) {
+                if(myString.charAt(i) == 'x') continue;
+                else {
+                    count++;
+                    break;
+                }
+            }
+            
+            char ch = myString.charAt(i);
+            if(ch == 'x') {
+                if(ch == myString.charAt(i + 1)) continue;
+            }
+            else {
+                if(myString.charAt(i + 1) == 'x') {
+                    count++;	
+            	}
+            }
+        }
+
+        answer = new String[count];
+        
+        for(int i = 0, j = 0; i < myString.length(); i++) {
+            if(i == myString.length() - 1) {
+                if(myString.charAt(i) == 'x') break;
+                else {
+                    temp += myString.charAt(i);
+                    answer[j] = temp;
+                    temp = "";
+                    break;
+                }
+            }
+            
+            char ch = myString.charAt(i);
+            
+            if(ch == 'x') {
+                if(ch == myString.charAt(i + 1)) continue;
+                else count++;
+            }
+            else {
+                if(myString.charAt(i+1) == 'x') {
+                    temp += myString.charAt(i);
+                    answer[j++] = temp;
+                    temp = "";
+                }
+                else {
+                	temp += myString.charAt(i);
+                }
+            }
+        }
+        
+        Arrays.sort(answer);
+        return answer;
+    }
+}
