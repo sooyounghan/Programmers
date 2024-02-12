d6599a084cb8d1778aa9e320772fbed3f5216c25
LoveWinsAll <34672301+sooyounghan@users.noreply.github.com>
2024-02-12 20:26:02 +0900

    [level 0] Title: 0 떼기, Time: 11.92 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int num = 0;
+        
+        for(int i = 0; i < n_str.length(); i++) {
+            int temp = n_str.charAt(i) - '0';
+        
+            if(temp == 0) {
+                if(i != n_str.length() - 1 && num > 0) num *= 10;
+                else continue;
+            }
+            
+            else {
+                num += temp;
+                if(i != n_str.length() - 1) num *= 10;
+                else continue;
+            }
+        }
+        
+        answer += num;
+        return answer;
+    }
+}
