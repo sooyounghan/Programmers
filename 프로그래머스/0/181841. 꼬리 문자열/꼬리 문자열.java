a95915b517089a9b04f957b985d7909f8710f33a
LoveWinsAll <34672301+sooyounghan@users.noreply.github.com>
2024-02-14 08:49:06 +0900

    [level 0] Title: 꼬리 문자열, Time: 1.23 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
@@ -1,14 +1,9 @@
 class Solution {
     public String solution(String[] str_list, String ex) {
         String answer = "";
-        boolean[] check = new boolean[str_list.length];
         
         for(int i = 0; i < str_list.length; i++) {
-            if(str_list[i].contains(ex)) check[i] = true;
-        }
-        
-        for(int i = 0; i < str_list.length; i++) {
-            if(!check[i]) answer += str_list[i];
+            if(!str_list[i].contains(ex)) answer += str_list[i];;
         }
         
         return answer;
