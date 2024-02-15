1e2fbe08812a2f86c6dfaa259808adfb78d1d6c9
LoveWinsAll <34672301+sooyounghan@users.noreply.github.com>
2024-02-15 16:31:08 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.08 ms, Memory: 67.9 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -1,39 +1,6 @@
 class Solution {
     public int[][] solution(int n) {
-    	int[][] answer = new int[n][n];
-        int x = 0;
-        int y = 0;
-        int num = 1;
-        int direction = 0;
-        
-        while(num != Math.pow(n, 2)) {
-            if(direction == 0) {
-                if(x == n - 1 || answer[y][x + 1] != 0) {
-                	answer[y][x] = num;
-                    direction = 1;
-                }
-                else answer[y][x++] = num++;
-            } else if(direction == 1) {
-                if(y == n - 1 || answer[y + 1][x] != 0) {
-                	answer[y][x] = num;
-                    direction = 2;
-                }
-                else answer[y++][x] = num++;
-            } else if(direction == 2) {
-                if(x == 0 || answer[y][x - 1] != 0) {
-                	answer[y][x] = num;
-                    direction = 3;
-                }
-                else answer[y][x--] = num++;
-            } else if(direction == 3) {
-                if(y == 0 || answer[y - 1][x] != 0) {
-                	answer[y][x] = num;
-                    direction = 0;
-                }
-                else answer[y--][x] = num++;
-            }
-        }
-        answer[y][x] = num;
+        int[][] answer = {};
         return answer;
     }
 }
