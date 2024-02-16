92e3b029990d9e0fe8bbb88a9221f2de7f3fa888
LoveWinsAll <34672301+sooyounghan@users.noreply.github.com>
2024-02-16 15:44:48 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int pizza = 1;
+        
+        while((pizza * 6) % n != 0) {
+            pizza++;
+        }
+        
+        answer = pizza;
+        return answer;
+    }
+}
