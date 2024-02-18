ded56c58e7b32ce9bdc9e374048449fb35c13e7e
LoveWinsAll <34672301+sooyounghan@users.noreply.github.com>
2024-02-18 16:52:01 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -1,17 +1,9 @@
 class Solution {
     public int solution(int[] numbers, int k) {
-        int answer = 0;
-        int num = 0;
-        int count = 0;
-            
-        while(count != k) {
-            answer = numbers[num];
-            
-            if((num + 2) >= numbers.length) num = (num + 2) % numbers.length;
-            else num = num + 2;
-            
-            count++;
-        }
+        int idx = ((2 * k) - 1) - 1;
+        if(idx >= numbers.length) idx %= numbers.length; 
+        int answer = numbers[idx];
+        
         return answer;
     }
 }
