7da0223822804645ba1b4a033b058c4b7e1feed7
LoveWinsAll <34672301+sooyounghan@users.noreply.github.com>
2024-02-22 09:28:27 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.13 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,25 +1,32 @@
 class Solution {
     public long solution(String numbers) {
         String[] number = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        String temp = "";
+        String substring_numbers = new String(numbers);
+        
+        String temp_num = null;
         long answer = 0;
         int end = 0;
         
-        for(int i = 0; i < numbers.length(); i++) {
-        	temp += numbers.charAt(i); // 성능 하락의 원인
-        	
-            switch(temp) {
-            	case "zero" : answer = (answer * 10) + 0; temp = ""; break; 
-            	case "one" : answer = (answer * 10) + 1; temp = ""; break;
-            	case "two" : answer = (answer * 10) + 2; temp = ""; break;
-            	case "three" : answer = (answer * 10) + 3; temp = ""; break;
-            	case "four" : answer = (answer * 10) + 4; temp = ""; break;
-            	case "five" : answer = (answer * 10) + 5; temp = ""; break;
-            	case "six" : answer = (answer * 10) + 6; temp = ""; break;
-            	case "seven" : answer = (answer * 10) + 7; temp = ""; break;
-            	case "eight" : answer = (answer * 10) + 8; temp = ""; break;
-            	case "nine" : answer = (answer * 10) + 9; temp = ""; break;
-            }
+        while(!substring_numbers.equals("")) {
+        	for(int i = 0; i < number.length; i++) {
+        		if(substring_numbers.startsWith(number[i])) {
+		        	end = number[i].length();
+		            temp_num = substring_numbers.substring(0, end);
+		            substring_numbers = substring_numbers.substring(end);
+		            switch(temp_num) {
+		            	case "zero" : answer = (answer * 10) + 0; break; 
+		            	case "one" : answer = (answer * 10) + 1; break;
+		            	case "two" : answer = (answer * 10) + 2; break;
+		            	case "three" : answer = (answer * 10) + 3; break;
+		            	case "four" : answer = (answer * 10) + 4; break;
+		            	case "five" : answer = (answer * 10) + 5; break;
+		            	case "six" : answer = (answer * 10) + 6; break;
+		            	case "seven" : answer = (answer * 10) + 7; break;
+		            	case "eight" : answer = (answer * 10) + 8; break;
+		            	case "nine" : answer = (answer * 10) + 9; break;
+		            }
+	        	}
+        	}
         }
     	return answer;
     }
