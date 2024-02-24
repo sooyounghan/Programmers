baedbb98c4dae2a7974453e9dc6d92f305ba94ac
LoveWinsAll <34672301+sooyounghan@users.noreply.github.com>
2024-02-24 17:20:04 +0900

    [level 0] Title: 다항식 더하기, Time: 7.00 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
@@ -7,48 +7,31 @@ class Solution {
 	        
 	        for(int i = 0; i < polynomial_arr.length; i++) {
 	            if(i == polynomial_arr.length - 1 && !polynomial_arr[i].contains("x")) {
-	            	if(polynomial_arr.length == 1) {
-	            		if(polynomial_arr[i].contains("x")) coefficient += polynomial_arr[i].charAt(0) - '0';
-	            		else number += Integer.parseInt(polynomial_arr[i]);
+	            	if(polynomial_arr.length == 1 || polynomial_arr[i - 1].equals("+")) {
+	            		number += Integer.parseInt(polynomial_arr[i]);
 	            		break;
 	            	}
-	            	else if(polynomial_arr[i - 1].equals("+")) {
-	                    number += Integer.parseInt(polynomial_arr[i]);
-	                    break;
-	                }
 	            }
 	            
 	            if(polynomial_arr[i].contains("x")) {
 	                String[] polynomial_x = polynomial_arr[i].split("x");
 	                
-	                if(polynomial_x.length == 0) {
-	                    coefficient += 1;
-	                }
-	                else {
-	                    coefficient += Integer.parseInt(polynomial_x[0]);
-	                }
+	                if(polynomial_x.length == 0) coefficient += 1;
+	                else coefficient += Integer.parseInt(polynomial_x[0]);
 	            }
 	            
-	            else if((i != 0 && polynomial_arr[i - 1].equals("+")) && polynomial_arr[i + 1].equals("+")) {
+	            else if((i != 0 && polynomial_arr[i - 1].equals("+")) && polynomial_arr[i + 1].equals("+")) 
 	                number += Integer.parseInt(polynomial_arr[i]);
-	            }
 	            
-	            else if(i == 0 && polynomial_arr[i + 1].equals("+")) {
+	            else if(i == 0 && polynomial_arr[i + 1].equals("+")) 
 	                number += Integer.parseInt(polynomial_arr[i]);
-	            }
 	        }
 	         
-	        if(coefficient == 0) {
-		        answer = String.valueOf(number);
-	        }
-	        else if(number == 0) {
-	        	if(coefficient == 1) answer = new String("x");
-	        	else answer = new String(coefficient + "x");
-	        }
-	        else {
-	        	if(coefficient == 1) answer = new String("x" + " + " + number);
-	        	else answer = new String(coefficient + "x" + " + " + number);
-	        }
+	        if(coefficient == 0) answer = String.valueOf(number);
+	        else if(number == 0) 
+	        	answer = (coefficient == 1) ? new String("x") : new String(coefficient + "x");
+	        else 
+	        	answer = (coefficient == 1) ? new String("x" + " + " + number) : new String(coefficient + "x" + " + " + number);
 	        return answer;
 	}
 }
