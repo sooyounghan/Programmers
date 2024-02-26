c8ba0d0568174a400b6ee1eb89bee34c6a819d96
lxx._.hxn <34672301+sooyounghan@users.noreply.github.com>
2024-02-27 03:19:12 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        int mod = 0;
+        int deno = 0;
+        
+        for(int i = 1; i <= a && i <= b; i++) {
+            if(a % i == 0 && b % i == 0) {
+                mod = i;
+            }
+        }
+        
+        deno = (b / mod);
+        
+        if(deno % 3 == 0) answer = 2;
+        else if(deno % 2 == 0 || deno % 5 == 0) {
+            while(true) {
+                if(deno % 2 == 0) deno /= 2;
+                else if(deno % 5 == 0) deno /= 5;
+                else break;
+            } 
+            if(deno == 1) answer = 1;
+            else answer = 2;
+        }
+        else if(deno == 1) answer = 1;
+        else answer = 2;
+        
+        return answer;
+    }
+}
